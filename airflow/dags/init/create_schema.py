276fa46b58de5aac7699387387ffd2f56e282ad8
GAURANG PANDYA <gaurangpandya@GAURANGs-MacBook-Pro.local>
2025-04-25 14:30:53 -0700

    using postgresoperator

--- airflow/dags/init/create_schema.py
+++ airflow/dags/init/create_schema.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-#from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime
 
@@ -9,10 +9,19 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
+    '''
     run_script = SQLExecuteQueryOperator(
         task_id='create_schema',
         sql="select * from ab_role;",
         conn_id='my_postgres_conn',
     )
+    '''
+    create_schema = PostgresOperator(
+        task_id='create_schema',
+        postgres_conn_id='my_postgres_conn',
+        sql="./sql/create_schema.sql"
+    )
 
-run_script
+
+
+create_schema
